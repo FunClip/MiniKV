46be22a5797f2f3de25bdeff716a045714bb3ea0
FunClip <ck770701780@hotmail.com>
2022-07-03 18:07:31 +0800

    Project-4: preparation of shared `KvsEngine`

--- src/kvse/mod.rs
+++ src/kvse/mod.rs
@@ -7,16 +7,16 @@ pub use self::kvs::KvStore;
 pub use self::sled::SledKvsEngine;
 
 /// Trait for a key value storage engine
-pub trait KvsEngine {
+pub trait KvsEngine: Clone + Send + 'static {
     /// Get the value of a given key
     /// Return `None` if the key does not exist
-    fn get(&mut self, key: String) -> Result<Option<String>>;
+    fn get(&self, key: String) -> Result<Option<String>>;
 
     /// Set the value of a given key
     /// If the key exists, the value will be overwritten
-    fn set(&mut self, key: String, value: String) -> Result<()>;
+    fn set(&self, key: String, value: String) -> Result<()>;
 
     /// Remove the value of a given key
     /// Return `KvsError::KeyNotFound` if the key does not exist
-    fn remove(&mut self, key: String) -> Result<()>;
+    fn remove(&self, key: String) -> Result<()>;
 }
